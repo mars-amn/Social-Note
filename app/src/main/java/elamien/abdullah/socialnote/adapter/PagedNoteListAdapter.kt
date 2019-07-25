c4f185363608179c418d5f8bcf1c04b1926dc7a8
AbduallahAtta <abdo.prog.point@gmail.com>
2019-07-25 15:45:53 +0200

    added the recyclerview-selection library with the proper implementation

--- app/src/main/java/elamien/abdullah/socialnote/adapter/PagedNoteListAdapter.kt
+++ app/src/main/java/elamien/abdullah/socialnote/adapter/PagedNoteListAdapter.kt
@@ -1,12 +1,21 @@
 package elamien.abdullah.socialnote.adapter
 
+import android.animation.TimeInterpolator
 import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.interpolator.view.animation.FastOutLinearInInterpolator
+import androidx.interpolator.view.animation.LinearOutSlowInInterpolator
 import androidx.paging.PagedListAdapter
+import androidx.recyclerview.selection.ItemDetailsLookup
+import androidx.recyclerview.selection.SelectionTracker
 import androidx.recyclerview.widget.RecyclerView
+import androidx.transition.Fade
+import androidx.transition.TransitionManager
+import androidx.transition.TransitionSet
+import com.transitionseverywhere.extra.Scale
 import elamien.abdullah.socialnote.database.Note
 import elamien.abdullah.socialnote.databinding.ListItemNotesBinding
 import elamien.abdullah.socialnote.ui.AddNoteActivity
@@ -19,6 +28,10 @@ import elamien.abdullah.socialnote.utils.NotesDiffCallback
  */
 class PagedNoteListAdapter(private val context : Context) :
     PagedListAdapter<Note, PagedNoteListAdapter.NotesViewHolder>(NotesDiffCallback()) {
+    var tracker : SelectionTracker<Long>? = null
+
+    override fun getItemId(position : Int) : Long = getNoteId(position)!!
+
     override fun onCreateViewHolder(parent : ViewGroup, viewType : Int) : NotesViewHolder {
         val inflater = LayoutInflater.from(context)
         val binding = ListItemNotesBinding.inflate(inflater, parent, false)
@@ -26,7 +39,14 @@ class PagedNoteListAdapter(private val context : Context) :
     }
 
     override fun onBindViewHolder(holder : NotesViewHolder, position : Int) {
-        holder.bind(getItem(position))
+        tracker?.let {
+            holder.bind(getItem(position), it.isSelected(getNoteId(position)))
+        }
+
+    }
+
+    fun getNoteId(position : Int) : Long? {
+        return getItem(position)?.id
     }
 
     inner class NotesViewHolder(var mBinding : ListItemNotesBinding) : RecyclerView.ViewHolder(mBinding.root) {
@@ -34,8 +54,32 @@ class PagedNoteListAdapter(private val context : Context) :
             mBinding.handlers = this
         }
 
-        fun bind(note : Note?) {
+        fun getItemDetails() : ItemDetailsLookup.ItemDetails<Long> =
+            object : ItemDetailsLookup.ItemDetails<Long>() {
+                override fun getPosition() : Int = adapterPosition
+                override fun getSelectionKey() : Long? = getNoteId(adapterPosition)
+            }
+
+        fun bind(note : Note?, isActivated : Boolean = false) {
             mBinding.note = note
+            mBinding.root.isActivated = isActivated
+
+            if (isActivated) {
+                setTransition(FastOutLinearInInterpolator())
+                mBinding.selectionIcon.visibility = View.VISIBLE
+
+            } else {
+                mBinding.selectionIcon.visibility = View.GONE
+                setTransition(LinearOutSlowInInterpolator())
+            }
+        }
+
+        private fun setTransition(interpolator : TimeInterpolator?) {
+            val set = TransitionSet()
+                .addTransition(Scale(0.9f))
+                .addTransition(Fade())
+                .setInterpolator(interpolator)
+            TransitionManager.beginDelayedTransition(mBinding.parent, set)
         }
 
         fun onNoteClick(view : View) {
--- app/src/main/java/elamien/abdullah/socialnote/ui/MainActivity.kt
+++ app/src/main/java/elamien/abdullah/socialnote/ui/MainActivity.kt
@@ -7,15 +7,20 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.paging.PagedList
+import androidx.recyclerview.selection.SelectionPredicates
+import androidx.recyclerview.selection.SelectionTracker
+import androidx.recyclerview.selection.StorageStrategy
 import elamien.abdullah.socialnote.R
 import elamien.abdullah.socialnote.adapter.PagedNoteListAdapter
 import elamien.abdullah.socialnote.database.Note
 import elamien.abdullah.socialnote.databinding.ActivityMainBinding
+import elamien.abdullah.socialnote.utils.NotesItemLookUp
+import elamien.abdullah.socialnote.utils.NotesKeyProvider
 import elamien.abdullah.socialnote.viewmodel.NoteViewModel
-import jp.wasabeef.recyclerview.adapters.AlphaInAnimationAdapter
 import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
+    var tracker : SelectionTracker<Long>? = null
 
     private lateinit var mBinding : ActivityMainBinding
     private val mViewModel : NoteViewModel by inject()
@@ -38,17 +43,44 @@ class MainActivity : AppCompatActivity() {
             })
     }
 
+    private fun addNotesToRecyclerView(t : PagedList<Note>?) {
+        showRecyclerView()
+        val adapter = PagedNoteListAdapter(this@MainActivity)
+        adapter.setHasStableIds(true)
+        adapter.submitList(t)
+        mBinding.notesRecyclerView.adapter = adapter
+        // AlphaInAnimationAdapter(adapter) it doesn't work properly with recyclerview selection library
+        setupTracker(adapter)
+    }
+
+    private fun setupTracker(adapter : PagedNoteListAdapter) {
+        tracker = SelectionTracker.Builder(
+            "notes-selection",
+            mBinding.notesRecyclerView,
+            NotesKeyProvider(mBinding.notesRecyclerView, adapter),
+            NotesItemLookUp(mBinding.notesRecyclerView),
+            StorageStrategy.createLongStorage()
+        )
+            .withSelectionPredicate(SelectionPredicates.createSelectAnything()).build()
+        adapter.tracker = tracker
+        addTrackerObserver()
+    }
+
+    private fun addTrackerObserver() {
+        tracker?.addObserver(
+            object : SelectionTracker.SelectionObserver<Long>() {
+                override fun onSelectionChanged() {
+                    super.onSelectionChanged()
+                    // ..todo( will be implemented )
+                }
+            })
+    }
+
     private fun hideRecyclerView() {
         mBinding.notesRecyclerView.visibility = View.GONE
         mBinding.noteEmptyStateLayout.visibility = View.VISIBLE
     }
 
-    private fun addNotesToRecyclerView(t : PagedList<Note>?) {
-        showRecyclerView()
-        val adapter = PagedNoteListAdapter(this@MainActivity)
-        adapter.submitList(t)
-        mBinding.notesRecyclerView.adapter = AlphaInAnimationAdapter(adapter)
-    }
 
     private fun showRecyclerView() {
         mBinding.lottieAnimationView.pauseAnimation()
