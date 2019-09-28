6d0756e32b9fcdbd72867535b486d42f6164bc3a
AbduallahAtta <abdo.prog.point@gmail.com>
2019-09-28 15:49:25 +0200

    solved the problem caused the coilloader not to show images in existed notes

--- app/src/main/java/playground/develop/socialnote/coilloader/CoilImageLoader.kt
+++ app/src/main/java/playground/develop/socialnote/coilloader/CoilImageLoader.kt
@@ -8,7 +8,6 @@ import android.util.DisplayMetrics
 import coil.Coil
 import coil.api.load
 import org.wordpress.aztec.Html
-import playground.develop.socialnote.coilloader.extensions.upscaleTo
 
 
 class CoilImageLoader(private val context: Context) : Html.ImageGetter {
@@ -17,38 +16,32 @@ class CoilImageLoader(private val context: Context) : Html.ImageGetter {
         loadImage(source, callbacks, maxWidth, 0)
     }
 
+
     override fun loadImage(source: String,
                            callbacks: Html.ImageGetter.Callbacks,
                            maxWidth: Int,
                            minWidth: Int) {
         Coil.load(context, source) {
             target { drawable ->
-                var bitmap: Bitmap?
-                if (drawable is BitmapDrawable) {
-                    if (drawable.bitmap != null) {
-                        bitmap = drawable.bitmap
-                    }
-                }
-
-                bitmap = if (drawable.intrinsicWidth <= 0 || drawable.intrinsicHeight <= 0) {
+                val bitmap: Bitmap? = if (drawable.intrinsicWidth <= 0 || drawable.intrinsicHeight <= 0) {
                     Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888)
                 } else {
                     Bitmap.createBitmap(drawable.intrinsicWidth,
                                         drawable.intrinsicHeight,
                                         Bitmap.Config.ARGB_8888)
                 }
-
-                val canvas = Canvas(bitmap)
-                drawable.setBounds(0, 0, canvas.width, canvas.height)
-                drawable.draw(canvas)
-
-                if (bitmap != null && bitmap.width < minWidth) {
-                    callbacks.onImageLoaded(BitmapDrawable(context.resources,
-                                                           bitmap.upscaleTo(minWidth)))
-                    return@target
+                if (drawable is BitmapDrawable) {
+                    if (drawable.bitmap != null) {
+                        drawable.bitmap
+                        callbacks.onImageLoaded(BitmapDrawable(context.resources, drawable.bitmap))
+                    }
+                } else {
+                    val canvas = Canvas(bitmap!!)
+                    drawable.setBounds(0, 0, canvas.width, canvas.height)
+                    drawable.draw(canvas)
+                    bitmap.density = DisplayMetrics.DENSITY_DEFAULT
+                    callbacks.onImageLoaded(BitmapDrawable(context.resources, bitmap))
                 }
-                bitmap.density = DisplayMetrics.DENSITY_DEFAULT
-                callbacks.onImageLoaded(BitmapDrawable(context.resources, bitmap))
             }
 
         }
